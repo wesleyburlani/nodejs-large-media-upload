228bfc10be66672a9b344dfc89cf2f4359a2bff5
wesleyburlani <wesleyburlani@gmail.com>
2024-01-11 08:52:07 -0300

    http upload with busboy

--- backend/index.ts
+++ backend/index.ts
@@ -1,16 +1,107 @@
 import 'express-async-errors';
 import express, { Request, Response  } from 'express';
 import http from 'http';
-
+import Busboy from 'busboy';
+import { Readable, Writable } from "stream";
+import ffmpeg from 'fluent-ffmpeg';
+import path from 'path';
+import { existsSync, mkdirSync } from 'fs';
+import { pipeline } from 'stream/promises';
 const PORT = 4000;
 
-const onError = (err: Error, req: Request, res: Response) => { 
-    res.status(500).json({ msg: err.message });
-}
-
 const app = express();
-app.use(onError);
 const server = http.createServer(app);
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.post('/', async (req: Request, res: Response) => {
+    console.log("uploading...");
+    const busboy = Busboy({ headers: req.headers });
+    busboy.on('file', (name: string, file: Readable, info: Busboy.FileInfo) => {
+        const { filename, encoding, mimeType } = info;
+        console.log(
+            `File [${name}]: filename: %j, encoding: %j, mimeType: %j`,
+            filename,
+            encoding,
+            mimeType
+          );
+          file.on('data', (data) => {
+            console.log(`File [${name}] got ${data.length} bytes`);
+          }).on('close', () => {
+            console.log(`File [${name}] done`);
+          });
+        const outputPath = `./tmp/upload/${new Date().getTime()}`;
+        if (!existsSync(outputPath)) {
+            mkdirSync(outputPath, { recursive: true });
+        }
+        const command = ffmpeg(file)
+        .on('start', function (commandLine) {
+            console.log('Starting the transcoding...');
+            console.log(commandLine);
+        })
+        .on('stderr', (stderrLine) => {
+            if (!stderrLine.includes('frame=')) {
+            console.error(stderrLine);
+            }
+        })
+        .on('error', (error) => {
+            console.error('Failed to complete transcoding', error);
+        })
+        .on('end', async function () {
+            console.log('Transcoding succeeded !');
+        })
+        .addOptions(['-hide_banner', '-y'])
+
+        command
+        .output(`${outputPath}/playlist.m3u8`)
+        .format('hls')
+        .complexFilter('scale=1280x720:force_original_aspect_ratio=decrease,pad=1280:720:-1:-1:color=black')
+        .outputOptions([
+            '-c:a aac',
+            '-b:a 160k',
+            '-ar 48000',
+            '-ac 2',
+            '-c:v libx264',
+            '-preset:v fast',
+            '-pix_fmt yuv420p',
+            '-crf 20',
+            '-maxrate 2808k',
+            '-bufsize 5616k',
+            '-max_muxing_queue_size 4096',
+            '-g 25',
+            '-keyint_min 25',
+            '-force_key_frames expr:gte(t,n_forced*1)',
+            `-hls_segment_filename ${outputPath}/chunk_%06d.ts`,
+            `-hls_time 2`,
+            '-hls_list_size 0',
+            '-master_pl_name master.m3u8',
+        ])
+        .run();
+    });
+
+    busboy.on('error', (err) => {
+        console.log(err);
+        res.status(500).send(err);
+    });
+    busboy.on('finish', () => {
+        res.status(200).send('ok');
+    });
+
+    try {
+        await pipeline(
+            req,
+            busboy
+        )
+    } catch (err) {
+        const error = err as Error
+        console.log('error**', error.stack)
+        return res.end(`Error!!: ${error.stack}`)
+    }
+});
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,85 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import { FFmpeg } from '@ffmpeg/ffmpeg';
 import './App.css';
 
 function App() {
+  const [loaded, setLoaded] = useState(false);
   const [message, setMessage] = useState('Click Start to transcode');
+  const [fileToUpload, setFileToUpload] = useState(null as File | null | undefined);
+  const ffmpegRef = useRef(new FFmpeg());
   
+  const load = async () => {
+    const ffmpeg = ffmpegRef.current;
+    ffmpeg.on('log', ({ message }) => {
+        console.log(message);
+    });
+    ffmpeg.on('progress', ({ progress, time }) => {
+      setMessage(`Encoding progress ${progress * 100} % (transcoded time: ${time / 1000000} s)`);
+    });
+    await ffmpeg.load();
+    setLoaded(true);
+  };
+
+  useEffect(() => {
+    load();
+  });
+
   const selectFile = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    setMessage('file selected');
+    const file = event.target.files?.item(0);
+    setFileToUpload(file);
   };
 
   const upload = async () => {
-    setMessage('Uploaded');
+    if (!fileToUpload) {
+      setMessage('No file selected');
+      return;
+    }
+
+    const ffmpeg = ffmpegRef.current;
+    const buffer = await fileToUpload.arrayBuffer();
+
+    const fileName = fileToUpload.name;
+    const fileNameWithoutExtension = fileName.substring(0, fileName.lastIndexOf('.'));
+    const outputFileName = `${fileNameWithoutExtension}.mkv`;
+
+    await ffmpeg.writeFile(fileName, new Uint8Array(buffer));
+    await ffmpeg.exec(['-i', fileName, '-c:a', 'copy', '-c:v', 'copy', outputFileName]);
+    const data = await ffmpeg.readFile(outputFileName);
+    setMessage('muxed');
+    const formData = new FormData();
+    formData.append("file", new Blob([data]));
+
+    const res = await fetch("http://localhost:4000", {
+      method: "POST",
+      body: formData,
+    }).then((res) => res.json());
   };
 
-  return (
+  const download = async (filePath: string) => {
+    const ffmpeg = ffmpegRef.current;
+    const data = await ffmpeg.readFile(filePath);
+    const a = document.createElement('a');
+    const dataUrl = window.URL.createObjectURL(new Blob([data]));
+    a.href = dataUrl;
+    const fileName = filePath.substring(filePath.lastIndexOf('/') + 1);
+    a.download = fileName;
+    a.click();
+    window.URL.revokeObjectURL(dataUrl);
+    a.remove();
+  };
+
+  return (loaded ? (
     <div className="App">
       <p/>
       <input type="file" accept="video/*,.mkv,audio/*" onChange={selectFile} />
       <button onClick={upload}>Start</button>
       <p>{message}</p>
     </div>
-  );
+  ):(
+    <div className="App">
+      <p>Loading...</p>
+    </div>
+  ));
 }
 
 export default App;
